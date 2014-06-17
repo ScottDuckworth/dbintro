5971ae74fc174638afb4f20243f7cb617342d6d1
Scott Duckworth <sduckwo@clemson.edu>
2014-06-17 12:54:20 -0400

    backport for compatibility with django 1.3

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -1,4 +1,8 @@
-from django.conf.urls import patterns, url
+import django
+if django.VERSION[:2] <= (1, 3):
+  from django.conf.urls.defaults import patterns, include, url
+else:
+  from django.conf.urls import patterns, include, url
 from . import views
 
 urlpatterns = patterns('',
